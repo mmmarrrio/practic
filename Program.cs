daaf4086c2d45339630df6c6d8dcb93a1b0b3503
mmmarrrio <84327171+mmmarrrio@users.noreply.github.com>
2025-02-19 20:14:26 +0300

    секонд уно

--- Program.cs
+++ Program.cs
@@ -1,28 +1,77 @@
-﻿//  задача 1 описание автомобиля
-using System.Threading.Channels;
-using System;
-
-public class Car
+﻿using System;
+using System.Runtime.CompilerServices;
+public class Calculator
 {
-    public string Brand { get; set; }
-    public string Name { get; set; }
-    public int Year { get; set; }
-    public Car(string Brand, string Name, int Year)
+    public int a { get; set; }
+    public int b { get; set; }
+    public Calculator(int a, int b)
     {
-        Brand = Brand;
-        Name = Name;
-        Year = Year;
+        this.a = a;
+        this.b = b;
     }
-    public void ShowInfo()
+    public void Subtract(int a, int b)
     {
-        Console.WriteLine(Brand + Name + Year);
+        Console.WriteLine($"choose wisly 1: {a}-{b} 2: {b}-{a}");
+        string t = Console.ReadLine();
+        if (t == "1")
+        {
+            int c = a - b;
+            Console.WriteLine(c);
+        }
+        else if (t == "2")
+        { 
+            int c = b - a;
+            Console.WriteLine(c);
+        }
+    }
+    public void Add(int a,int b) 
+    {
+        int c = a + b;
+        Console.WriteLine(c);
+    }
+    public void Multiply(int a,int b) 
+    {
+        int c = a * b;
+        Console.WriteLine(c);
+    }
+    public void Divide(int a, int b)
+    {
+        Console.WriteLine($"choose wisly 1: {a}/{b} 2: {b}/{a}");
+        string t = Console.ReadLine();
+        if (t == "1")
+        {
+            if (b != 0)
+            {
+                int c = a / b;
+                Console.WriteLine(c);
+            }
+            else 
+            { 
+                Console.WriteLine("actually i can't let you do this but... i think nobody is watching for us so, it's infinity");
+            }
+        }
+        else if (t == "2")
+        {
+            if (a != 0)
+            {
+                int c = b / a;
+                Console.WriteLine(c);
+            }
+            else 
+            { 
+                Console.WriteLine("actually i can't let you do this but... i think nobody is watching for us so, it's infinity"); 
+            }
+        }
     }
 }
 class Programm
 {
     static void Main(string[] args)
     {
-        Car car = new Car("zov"+"goyda"+123);
-        Console.WriteLine(car.Brand+car.Name+car.Year);
+        Calculator calc = new Calculator(1,2);
+        calc.Add(1,3);
+        calc.Subtract(2,4);
+        calc.Divide(3,5);
+        
     }
 }
