c5cac07d5b729acb7177f6b2eb62cf800fd75bd3
mmmarrrio <84327171+mmmarrrio@users.noreply.github.com>
2025-02-19 19:21:34 +0300

    практик 1

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿//  задача 1 описание автомобиля
+using System.Threading.Channels;
+using System;
+
+public class Car
+{
+    public string Brand { get; set; }
+    public string Name { get; set; }
+    public int Year { get; set; }
+    public Car(string Brand, string Name, int Year)
+    {
+        Brand = Brand;
+        Name = Name;
+        Year = Year;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine(Brand + Name + Year);
+    }
+}
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car("zov"+"goyda"+123);
+        Console.WriteLine(car.Brand+car.Name+car.Year);
+    }
+}
